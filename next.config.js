dc047b59f29e4806d72b0ef2178258933ff5c6b2
Your Name <irene.germani@kcl.ac.uk>
2025-11-26 12:48:07 +0000

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withOffline = require('next-offline');
 
 const nextConfig = {
-	poweredByHeader: false,
-	swcMinify: true,
+  poweredByHeader: false,
+  swcMinify: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = withOffline(nextConfig);
-
